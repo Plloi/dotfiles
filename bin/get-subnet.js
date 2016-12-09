bc64a2335bfb1e496de5b90c09ed79c17bf56727
Shaun Hammill <plloi.pllex@gmail.com>
2016-12-09 11:47:50 -0500

    Meep

--- bin/get-subnet.js
+++ bin/get-subnet.js
@@ -2,8 +2,9 @@ let getSubnet = abbr=>
 {
 	let alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-	if (abbr.length > 3)
-		return "0.0"
-
-	return abbr.split('').map(c=>('00000'+(alphabet.indexOf(c.toLowerCase())+1).toString(2)).slice(-5)).join('').match(/.{1,8}/g).map(oct=>Number.parseInt(oct,2))
+	return abbr.length > 3?
+		"0.0" :
+		abbr.split('').map(c=>('00000'+(alphabet.indexOf(c.toLowerCase())+1).toString(2)).slice(-5)).join('').match(/.{1,8}/g).map(oct=>Number.parseInt(oct,2)).join('.')
 }
+
+console.log(getSubnet(process.argv[2]))
