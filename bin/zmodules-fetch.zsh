1d21758d21c2fd29ad728a293a1999335976ac49
Shaun Hammill <plloi.pllex@gmail.com>
2018-01-15 11:14:07 -0500

    start zim module management

---
+++ bin/zmodules-fetch.zsh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+zmodload zsh/mapfile
+pushd ${ZIM_HOME}/modules
+pwd
+
+local zimmodfile=~/.zmodules
+local zimmodules=( "${(f)mapfile[$zimmodfile]}" )
+local zmodule
+
+
+for zmodule in $zimmodules; do
+  zmodule=("${(@s/ /)zmodule}") 
+  if [[ ! -d ${ZIM_HOME}/modules/${zmodule[1]} ]]; then
+    print "${zmodule[1]} Not found, attempting look and install..."
+    if (( ${+zmodule[2]} )); then
+      git clone ${zmodule[2]} ${zmodule[1]}
+    else
+      print "${zmodule[1]} has no repository defined."
+    fi
+  else
+    print "${zmodule[1]} Exists, skpping..."
+  fi
+done
+popd
