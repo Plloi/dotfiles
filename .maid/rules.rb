7cd44a3eabc4ed1ad862b08aac6d6d1de281e696
codefactor-io <support@codefactor.io>
2019-09-11 14:50:19 +0000

    [CodeFactor] Apply fixes

--- .maid/rules.rb
+++ .maid/rules.rb
@@ -8,11 +8,11 @@
 # what they do, you might run into unwanted results!
 #
 # Don't forget, it's just Ruby!  You can define custom methods and use them below:
-# 
+#
 #     def magic(*)
 #       # ...
 #     end
-# 
+#
 # If you come up with some cool tools of your own, please send me a pull request on GitHub!  Also, please consider sharing your rules with others via [the wiki](https://github.com/benjaminoakes/maid/wiki).
 #
 # For more help on Maid:
